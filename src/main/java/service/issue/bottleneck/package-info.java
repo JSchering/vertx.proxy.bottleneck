b274de1a0492433b30cecfad1b6473bffbf78ba2
JSchering <scheringjm@gmail.com>
2016-12-13 08:53:00 -0800

    Revert "reproducer"
    This reverts commit d27242ac18831d9d82085a673ed737f5f86b86ac.

--- src/main/java/service/issue/bottleneck/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Indicates that this module contains classes that need to be generated / processed.
- */
-@ModuleGen(name = "test-service", groupPackage = "service.issue.bottleneck")
-
-package service.issue.bottleneck;
-
-import io.vertx.codegen.annotations.ModuleGen;
